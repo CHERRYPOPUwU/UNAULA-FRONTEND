af97d986c4b0c8c5187ff6eb8e21d7c0a89d7624
Emanuel Pulgarin <erXiplay@gmail.com>
2025-05-12 09:57:13 -0500

    Feat(Components): Add HeaderSection Component with styles

---
+++ src/components/HeaderSection.jsx
@@ -0,0 +1,17 @@
+// src/components/HeaderSection.jsx
+import { Button } from "./Button"
+import "../styles/components/HeaderSection.css";
+
+export default function HeaderSection({ title, onCreateClick, createLabel = "Create" }) {
+  return (
+    <div className="header-section">
+      <h2 className="32px">{title}</h2>
+      <Button
+        className="" 
+        onClick={onCreateClick}
+      >
+        {createLabel}
+      </Button>
+    </div>
+  );
+}
